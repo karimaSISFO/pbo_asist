137a3fa99fba5a0ce05637949c6e26b578518e8c
nono <rmrima82@gmail.com>
2026-08-26 16:35:00 +0800

    perbaiki fitur bug

--- BookService.java
+++ BookService.java
@@ -1,5 +1,5 @@
 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+//nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package b1prak713120240020.book;
