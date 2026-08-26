985a7d35929b9da4a6c4f4c58db00f12c76c12b5
nono <rmrima82@gmail.com>
2026-08-26 16:43:18 +0800

    bugger

--- BookServiceImpl.java
+++ BookServiceImpl.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-st/SystemFileSystem/Templates/Classes/Class.java to edit this template
+m/Templates/Classes/Class.java to edit this template
 
 package b1prak713120240020.book;
 
