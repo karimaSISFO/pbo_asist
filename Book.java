67f6423405d767a7511fdaa1f18a2aca99fddd25
nono <rmrima82@gmail.com>
2026-08-26 16:32:18 +0800

    update fitur
    Removed license comment from Book.java

--- Book.java
+++ Book.java
@@ -1,5 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/
 
  
 package b1prak713120240020.book;
