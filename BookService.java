1f232cbe56cdb0396f594d6a429f75431a482e59
nono <rmrima82@gmail.com>
2026-08-26 16:36:04 +0800

    remove bug

--- BookService.java
+++ BookService.java
@@ -1,5 +1,4 @@
-
-emplates/Licenses/license-default.txt to change this license
+txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  
 package b1prak713120240020.book;
