77509d2afe3916f7b90cc0f64cac81592892fffd
nono <rmrima82@gmail.com>
2026-08-26 16:42:21 +0800

    icon bug

--- BookServiceImpl.java
+++ BookServiceImpl.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package b1prak713120240020.book;
 
 /**
